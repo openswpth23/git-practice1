739fb52f14bd3c447de88161aff69e282aff1607
openswpth23 <yugio_@naver.com>
2023-05-01 17:17:38 +0900

    feat: Python env test
    Create do_fizzbuzz and test.

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def do_fizzbuzz():
+    print('hello')
+    """
+    fizzbuzz: print fizz buzz fizzbuzz
+    3: fizz
+    5:buzz
+    15: fizzbuzz
+    """
+
+
+if __name__=='__main__':
+    do_fizzbuzz()
