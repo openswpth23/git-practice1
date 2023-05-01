8e78bcee42fac3841a8dd54a0fc040403f92eb88
openswpth23 <yugio_@naver.com>
2023-05-01 17:25:46 +0900

    feat: Done fizzbuzzPrint fizzbuzz if i is multiple of 15.
    Condition: i is multiple of 3.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
-def do_fizzbuzz():
-    print('hello')
-    """
-    fizzbuzz: print fizz buzz fizzbuzz
-    3: fizz
-    5:buzz
-    15: fizzbuzz
-    """
+def do_fizzbuzz(num:int):
+    """fizzbuzz: print fizz buzz fizzbuzz3: fizz5: buzz15: fizzbuzzetc: num"""
 
-
+    for i in range(1,num+1):
+        if i%15==0:print('fizzbuzz')
+        elif i%3==0:print('fizz')
+        elif i%5==0:print('buzz')
+        else:
+            print(f'{i}')
 if __name__=='__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
+
